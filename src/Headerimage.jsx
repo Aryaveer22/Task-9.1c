5a2f9c1c91a9ee2d558294eeab140c9d7b04d5dc
Aryaveer Jain <111032260+Aryaveer22@users.noreply.github.com>
2024-09-29 22:33:34 +1000

    Create Headerimage.jsx

---
+++ src/Headerimage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HeaderImage.css';
+
+const HeaderImage = () => (
+  <div>
+    <img className="header-image" src="./images/4.png" alt="Header" />
+  </div>
+);
+
+export default HeaderImage;

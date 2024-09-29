3bc696055f2a32e2560e01352a9e935a7724b1cb
Aryaveer Jain <111032260+Aryaveer22@users.noreply.github.com>
2024-09-29 22:32:48 +1000

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-links">
+        <div>
+          <h4>About Us</h4>
+          <p>FOOTER</p>
+        </div>
+        <div>
+          <h4>Contact</h4>
+          <p>Contact info</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
